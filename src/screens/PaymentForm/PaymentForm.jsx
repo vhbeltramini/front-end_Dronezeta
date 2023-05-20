9e6c08c0149625718b3b7142258d3eeb43ea6b9f
André E. Zanella <70669215+AndreElliasZanella@users.noreply.github.com>
2023-05-20 20:20:45 -0300

    update

--- src/screens/PaymentForm/PaymentForm.jsx
+++ src/screens/PaymentForm/PaymentForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import './PaymentForm.css';
 import axios from 'axios';
+import './PaymentForm.css';
 
 function PaymentForm() {
     const [cardNumber, setCardNumber] = useState('');
@@ -13,13 +13,13 @@ function PaymentForm() {
 
     useEffect(() => {
         const fetchPaymentMethods = async () => {
-            try {
-                const response = await axios.get(`http://localhost:8080/users/${user.id}`);
-                const paymentMethodList = response.data.paymentMethodList;
-                setPaymentMethods(paymentMethodList);
-            } catch (error) {
-                console.error('Erro ao obter os métodos de pagamento:', error);
-            }
+        try {
+            const response = await axios.get(`http://localhost:8080/users/${user.id}`);
+            const paymentMethodList = response.data.paymentMethodList;
+            setPaymentMethods(paymentMethodList);
+        } catch (error) {
+            console.error('Erro ao obter os métodos de pagamento:', error);
+        }
         };
 
         fetchPaymentMethods();
@@ -41,90 +41,94 @@ function PaymentForm() {
         setCvv(event.target.value);
     };
 
+    const totalPedidos = (cart) => {
+        const total = cart.reduce((sum, item) => sum + item.price, 0);
+        console.log('TOTAL ------------------------')
+        console.log(total)
+        return total;
+    };
+
     const handleSubmit = async (event) => {
         event.preventDefault();
-       
+
         try {
-            //gravar forma de pagamento
-            const response = await axios.post('http://localhost:8080/payment-methods/create', {
-                cardNumber,
-                cardHolderName,
-                validDate,
-                cvv,
-                id: user.id
-            });
-            console.log('Resposta do servidor:', response.data);
-            //gravar lista de produtos do pedido
-            // const response2 = await axios.post('http://localhost:8080/orders', {
-            //     cardNumber,
-            //     cardName,
-            //     expiryDate,
-            //     cvv,
-            //     id: user.id
-            // });
-            // //gravar pedido completo
-            // const response3 = await axios.post('http://localhost:8080/orders', {
-            //     cardNumber,
-            //     cardName,
-            //     expiryDate,
-            //     cvv,
-            //     id: user.id
-            // });
-            
-            // Limpar os campos após o envio
-            setCardNumber('');
-            setCardName('');
-            setExpiryDate('');
-            setCvv('');
+        // gravar forma de pagamento
+        console.log(user.id);
+        const response = await axios.post('http://localhost:8080/payment-methods/create', {
+            cardNumber,
+            cardHolderName,
+            validDate,
+            cvv,
+            user: user.id
+        });
+        console.log('CADASTRO FORMA PAGAMENTO - Resposta do servidor:', response.data);
+
+        // gravar lista de produtos do pedido
+        const response2 = await axios.post('http://localhost:8080/orders', {
+            products: cart,
+            date: new Date(),
+            user: user.id,
+            paymentMethod: response.data.id,
+            status: 'READY',
+            delivery: null,
+            totalValue: totalPedidos(cart)
+        });
+        console.log('CADASTRO ORDEM - Resposta do servidor:', response2.data);
+
+        // Limpar os campos após o envio
+        setCardNumber('');
+        setCardName('');
+        setExpiryDate('');
+        setCvv('');
         } catch (error) {
-            console.error('Erro ao enviar os dados:', error);
+        console.error('Erro ao enviar os dados:', error);
         }
     };
 
     return (
         <div className='payment-form'>
-            <h2>Forma de Pagamento</h2>
-            <h3>Métodos de Pagamento Salvos:</h3>
-            <ul>
-                {paymentMethods.map((method) => (
-                    <li key={method.id}>{method.name}</li>
-                ))}
-            </ul>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Número do Cartão:
-                    <input
-                        type="text"
-                        value={cardNumber}
-                        onChange={handleCardNumberChange}
-                    />
-                </label>
-                <label>
-                    Nome no Cartão:
-                    <input
-                        type="text"
-                        value={cardHolderName}
-                        onChange={handleCardNameChange}
-                    />
-                </label>
-                <label>
-                    Data de Expiração:
-                    <input
-                        type="text"
-                        value={validDate}
-                        onChange={handleExpiryDateChange}
-                    />
-                </label>
-                <label>
-                    CVV:
-                    <input
-                        type="text"
-                        value={cvv}
-                        onChange={handleCvvChange}
-                    />
-                </label>
-                <button type="submit">Confirmar Pagamento</button>
-            </form>
+        <h2>Forma de Pagamento</h2>
+        <h3>Métodos de Pagamento Salvos:</h3>
+        <ul>
+            {paymentMethods.map((method) => (
+            <li key={method.id}>{method.name}</li>
+            ))}
+        </ul>
+        <form onSubmit={handleSubmit}>
+            <label>
+            Número do Cartão:
+            <input
+                type="text"
+                value={cardNumber}
+                onChange={handleCardNumberChange}
+            />
+            </label>
+            <label>
+            Nome no Cartão:
+            <input
+                type="text"
+                value={cardHolderName}
+                onChange={handleCardNameChange}
+            />
+            </label>
+            <label>
+            Data de Expiração:
+            <input
+                type="text"
+                value={validDate}
+                onChange={handleExpiryDateChange}
+            />
+            </label>
+            <label>
+            CVV:
+            <input
+                type="text"
+                value={cvv}
+                onChange={handleCvvChange}
+            />
+            </label>
+            <button type="submit">Confirmar Pagamento</button>
+        </form>
         </div>
     );
 }
