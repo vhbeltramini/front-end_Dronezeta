63197207bbe150dc8dc790e501cd5f3f27a83f28
André E. Zanella <70669215+AndreElliasZanella@users.noreply.github.com>
2023-05-20 17:12:59 -0300

    update header

--- src/screens/components/header/header.jsx
+++ src/screens/components/header/header.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import './Header.css';
 import logo from  '../../../assets/images/shopping-cart.png'
+import profileIcon from '../../../assets/images/profile.png';
 
 function Header() {
 
   const redirectToCart = () => {
-    window.location.href = "/Cart";
+    window.location.href = "/cart";
   };
 
   const redirectToHome = () => {
-    window.location.href = "/";
+    window.location.href = "/"
   };
+
+  const redirectToProfile = () => {
+    window.location.href = "/userProfile"
+  };
+
+  const LoggedUser = JSON.parse(localStorage.getItem('user'));
 
   return (
     <header>
@@ -18,6 +25,11 @@ function Header() {
       <div className="header-buttons">
         <a href="/products" className="header-button">Produtos</a>
         <a href="/login" className="header-button">Login</a>
+        {LoggedUser?.id && (
+          <div className="profile-icon" onClick={redirectToProfile}>
+            <img src={profileIcon} className="profile-icon" alt="Ícone do Perfil" />
+          </div>
+        )}
         <div className="cart-icon" onClick={redirectToCart}>
           <img src={logo} className="cart-icon" alt="Ícone do Carrinho" />
         </div>
